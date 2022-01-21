31e011b18bd890e52c0acb4c5d2208bdbb901ce6
nuxsh <nuxshed@gmail.com>
2022-01-21 12:31:44 +0530

    add placeholder blog post

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+  scroll();
+};
+
+function scroll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
